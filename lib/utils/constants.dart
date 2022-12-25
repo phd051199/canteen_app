fa201eed26880a6bcc48b4a41049386ff6620c4f
Phạm Duy <PHD051199@gmail.com>
2022-12-25 04:00:00 +0000

    Update

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const apiURL = 'https://phamduy.me';
+const apiURL = 'http://phamduy.me:8080';
 
 const primaryBGColor = Color(0xffF8F5F2);
 const secondaryBGColor = Color(0xffE84C4F);
